969c71f2a7c8b14b68862084da867133140c7682
Dipti Rani <gdipti642@gmail.com>
2021-08-10 13:05:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,26 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      markdown: '# Heading' + '\n'
-
-      + ' ## sub-heading...' + '\n' +
-      '[links](https://www.freecodecamp.org)' + '\n' +  '\n' +
-      'Inline code `<div></div>`' + '\n' + '\n' +
-      ' > Block Quotes!' + '\n' + '\n' +
-      '**Bolded text**' + '\n' + '\n' +
-      ' > Block Quotes!' + '\n' +
-      '- item1' + '\n' +
-      '- item2' + '\n' +
-      '- item3' + '\n' +
+      markdown: '# Heading' + 
+      '\n' +
+      '## sub-heading...' +
+      '\n' +
+      '[links](https://www.freecodecamp.org)' +
+      '\n\n' + 
+      'Inline code `<div></div>`' +
+      '\n\n' +
+      ' > Block Quotes!' +
+      '\n\n' +
+      '**Bolded text**' +
+      '\n\n' +
+      ' > Block Quotes!' + 
+      '\n' +
+      '- item1' +
+      '\n' +
+      '- item2' +
+      '\n' +
+      '- item3' +
+      '\n' +
       '![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)'
             
     }
