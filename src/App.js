a94e23b63b5c9861f80929efc808189c22366f78
Dipti Rani <gdipti642@gmail.com>
2021-08-10 11:20:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import marked from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      // input: ''
+      markdown: ""
+    }
+  }
+
+  updateMarkdown (markdown){
+    this.setState({
+      // input: e.target.value
+      markdown
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Markdown Previewer</h1>
+        <div className="container">
+
+          <div class="card">
+            <div class="card-header bg-info">
+              Editor
+            </div>
+              {/* <textarea id="editor" onChange={this.handleChange.bind(this)}></textarea> */}
+              <textarea id="editor" value={this.state.markdown}
+                  onChange={(e) => {
+                    this.updateMarkdown(e.target.value);
+                  }}></textarea>
+
+          </div>
+
+          <div class="card">
+            <div class="card-header bg-info">
+              Previewer
+            </div>
+              {/* <div id="preview">{this.state.input}</div> */}
+              <div id="preview" dangerouslySetInnerHTML={{
+                  __html: marked(this.state.markdown),
+                }}></div> 
+          </div>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
